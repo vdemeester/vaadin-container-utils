cb2735a5d9485f1dab9667f623819352da66e131
Vincent Demeester <vincent+git@demeester.fr>
2012-07-16 12:04:57 +0200

    Add PropertyMetadata

---
+++ src/main/java/org/shortbrain/vaadin/container/PropertyMetadata.java
@@ -0,0 +1,62 @@
+package org.shortbrain.vaadin.container;
+
+/**
+ * Metedata used to create a property of a Vaadin Container.
+ * 
+ * @author Vincent Demeester <vincent@demeeester.fr>
+ * 
+ */
+public class PropertyMetadata {
+
+	/**
+	 * Name of the property.
+	 */
+	private final String propertyName;
+	/**
+	 * Type of the property.
+	 */
+	private final Class<?> propertyClass;
+	/**
+	 * Default value of the property.
+	 */
+	private final Object defaultValue;
+	/**
+	 * Attribute of the property – might be used to get the value of the
+	 * property.
+	 */
+	private final String propertyAttribute;
+
+	/**
+	 * Creates a {@link PropertyMetadata}
+	 * 
+	 * @param propertyName
+	 * @param propertyClass
+	 * @param defaultValue
+	 * @param propertyAttribute
+	 */
+	public PropertyMetadata(String propertyName, Class<?> propertyClass,
+			Object defaultValue, String propertyAttribute) {
+		super();
+		this.propertyName = propertyName;
+		this.propertyClass = propertyClass;
+		this.defaultValue = defaultValue;
+		this.propertyAttribute = propertyAttribute;
+	}
+
+	public String getPropertyName() {
+		return propertyName;
+	}
+
+	public Class<?> getPropertyClass() {
+		return propertyClass;
+	}
+
+	public Object getDefaultValue() {
+		return defaultValue;
+	}
+
+	public String getPropertyAttribute() {
+		return propertyAttribute;
+	}
+
+}
