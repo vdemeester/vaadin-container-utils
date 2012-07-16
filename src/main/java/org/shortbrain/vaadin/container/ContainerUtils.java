ab847b0e1c6a200dcb1c1cb8a6d18fcabb897404
Vincent Demeester <vincent+git@demeester.fr>
2012-07-16 11:53:08 +0200

    Update ContainerUtils

--- src/main/java/org/shortbrain/vaadin/container/ContainerUtils.java
+++ src/main/java/org/shortbrain/vaadin/container/ContainerUtils.java
@@ -1,6 +1,5 @@
 package org.shortbrain.vaadin.container;
 
-
 import com.vaadin.data.Container;
 import com.vaadin.data.Container.Filterable;
 import com.vaadin.data.Container.Hierarchical;
@@ -9,43 +8,81 @@ import com.vaadin.data.util.IndexedContainer;
 
 /**
  * Util classes for vaadin Container.
- *
+ * 
  * @author Vincent Demeester <vincent.demeester@nextep.net>s
- *
+ * 
  */
 public final class ContainerUtils {
 
-    /**
-     * Private constructor to force the non-instantiation of this class.
-     */
-    private ContainerUtils() {
-    }
+	/**
+	 * Private constructor to force the non-instantiation of this class.
+	 */
+	private ContainerUtils() {
+	}
 
-    /**
-     * Initialize a container with the given type.
-     *
-     * @param containerClass
-     *            type of the container
-     * @throws IllegalAccessException
-     * @throws InstantiationException
-     */
-    public static Container initContainer(Class<? extends Container> containerClass)
-            throws InstantiationException, IllegalAccessException {
-        Container container = null;
-        if (containerClass.isInterface()) {
-            if (containerClass == Filterable.class) {
-                // Default Filterable is Indexed
-                container = new IndexedContainer();
-            } else if (containerClass == Hierarchical.class) {
-                // Default Hierarchical is HierarchicalContainer
-                container = new HierarchicalContainer();
-            } else {
-                throw new InstantiationException(containerClass
-                        + " not supported.");
-            }
-        } else {
-            container = containerClass.newInstance();
-        }
-        return container;
-    }
+	/**
+	 * Initialize a container with the given type.
+	 * 
+	 * @param containerClass
+	 *            type of the container
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 */
+	public static Container initContainer(
+			Class<? extends Container> containerClass)
+			throws InstantiationException, IllegalAccessException {
+		Container container = null;
+		if (containerClass.isInterface()) {
+			if (containerClass == Filterable.class) {
+				// Default Filterable is Indexed
+				container = new IndexedContainer();
+			} else if (containerClass == Hierarchical.class) {
+				// Default Hierarchical is HierarchicalContainer
+				container = new HierarchicalContainer();
+			} else {
+				throw new InstantiationException(containerClass
+						+ " not supported.");
+			}
+		} else {
+			container = containerClass.newInstance();
+		}
+		return container;
+	}
+
+	/**
+	 * Add a property to the given container using {@link PropertyMetadata}.
+	 * 
+	 * @see ContainerUtils#addContainerProperty(Container, Object, Class,
+	 *      Object)
+	 * 
+	 * @param container
+	 *            the container.
+	 * @param propertyMetadata
+	 *            the porperty metadata.
+	 */
+	public static void addContainerProperty(Container container,
+			PropertyMetadata propertyMetadata) {
+		addContainerProperty(container, propertyMetadata.getPropertyName(),
+				propertyMetadata.getPropertyClass(),
+				propertyMetadata.getDefaultValue());
+	}
+
+	/**
+	 * Add a property to the given container.
+	 * 
+	 * @param container
+	 *            the container.
+	 * @param propertyName
+	 *            the name of the property.
+	 * @param propertyClass
+	 *            the type of the property.
+	 * @param propertyDefaultValue
+	 *            the default value of the propertyx
+	 */
+	public static void addContainerProperty(Container container,
+			Object propertyName, Class<?> propertyClass,
+			Object propertyDefaultValue) {
+		container.addContainerProperty(propertyName, propertyClass,
+				propertyDefaultValue);
+	}
 }
